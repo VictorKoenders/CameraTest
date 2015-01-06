3ff2d16d9fef2a747bfc7d5cedde2dfd6d4e4626
Victor Koenders <victor.koenders@gmail.com>
2015-01-06 13:21:06 -0800

    Initial commit

---
+++ CameraTest/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+
+namespace CameraTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.lstVideoDevices = new System.Windows.Forms.ComboBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// lstVideoDevices
+			// 
+			this.lstVideoDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.lstVideoDevices.FormattingEnabled = true;
+			this.lstVideoDevices.Location = new System.Drawing.Point(13, 13);
+			this.lstVideoDevices.Name = "lstVideoDevices";
+			this.lstVideoDevices.Size = new System.Drawing.Size(502, 21);
+			this.lstVideoDevices.TabIndex = 0;
+			this.lstVideoDevices.SelectedIndexChanged += new System.EventHandler(this.lstVideoDevices_SelectedIndexChanged);
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(13, 40);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(502, 372);
+			this.panel1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(527, 424);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.lstVideoDevices);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox lstVideoDevices;
+        private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ CameraTest/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Expression.Encoder.Devices;
+using Microsoft.Expression.Encoder.Live;
+using MODI;
+
+namespace CameraTest
+{
+	public partial class Form1 : Form
+	{
+		private readonly LiveJob _job;
+		private LiveDeviceSource _deviceSource;
+		private bool _isRunning = true;
+
+		private readonly Thread _imageThread;
+
+		public Form1()
+		{
+			InitializeComponent();
+			_job = new LiveJob();
+
+			foreach (EncoderDevice edv in EncoderDevices.FindDevices(EncoderDeviceType.Video))
+			{
+				lstVideoDevices.Items.Add(edv.Name);
+			}
+
+			lstVideoDevices.SelectedIndex = 0;
+
+			lstVideoDevices_SelectedIndexChanged(lstVideoDevices, new EventArgs());
+
+			_imageThread = new Thread(CheckImage);
+			_imageThread.Start();
+		}
+
+		delegate Bitmap GetPanelBitmapCallback(Panel panel);
+
+		private Bitmap GetPanelBitmap(Panel panel)
+		{
+			// InvokeRequired required compares the thread ID of the 
+			// calling thread to the thread ID of the creating thread. 
+			// If these threads are different, it returns true. 
+			if (panel.InvokeRequired)
+			{
+				GetPanelBitmapCallback d = GetPanelBitmap;
+				try
+				{
+					return Invoke(d, panel) as Bitmap;
+				}
+				catch
+				{
+					return null;
+				}
+			}
+			Rectangle rectangle = panel1.Bounds;
+			Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height);
+			using (Graphics g = Graphics.FromImage(bitmap))
+			{
+				Point source = panel1.PointToScreen(new Point(panel1.ClientRectangle.X, panel1.ClientRectangle.Y));
+				g.CopyFromScreen(source, Point.Empty, rectangle.Size);
+			}
+			return bitmap;
+		}
+
+		private static readonly Regex WhiteSpaceRegex = new Regex("[\t\n\r ]", RegexOptions.Compiled);
+
+		private void CheckImage()
+		{
+			if (!Directory.Exists("Images"))
+			{
+				Directory.CreateDirectory("Images");
+			}
+			while (_isRunning)
+			{
+				Thread.Sleep(1);
+				using (Bitmap bitmap = GetPanelBitmap(panel1))
+				{
+					if (bitmap == null)
+					{
+						continue;
+					}
+
+					Guid guid = Guid.NewGuid();
+					string image = "Images/" + guid + ".png";
+
+					bitmap.Save(image, ImageFormat.Png);
+
+					try
+					{
+						Document modiDocument = new Document();
+						modiDocument.Create(image);
+						modiDocument.OCR(MiLANGUAGES.miLANG_ENGLISH);
+						MODI.Image modiImage = (modiDocument.Images[0] as MODI.Image);
+						if (modiImage != null)
+						{
+							string extractedText = modiImage.Layout.Text;
+
+							Console.WriteLine(extractedText.Replace("\n", "\\n"));
+						}
+						modiDocument.Close();
+					}
+					catch (Exception ex)
+					{
+					}
+
+				}
+
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			_isRunning = false;
+			Thread.Sleep(500);
+			_imageThread.Interrupt();
+			_imageThread.Join();
+		}
+
+		private void lstVideoDevices_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			EncoderDevice video = EncoderDevices.FindDevices(EncoderDeviceType.Video).FirstOrDefault(d => d.Name == lstVideoDevices.SelectedItem as string);
+
+			if (_deviceSource != null)
+			{
+				_job.RemoveDeviceSource(_deviceSource);
+				_deviceSource = null;
+			}
+
+			if (video == null)
+			{
+				return;
+			}
+			// Create a new device source. We use the first audio and video devices on the system
+			_deviceSource = _job.AddDeviceSource(video, null);
+
+			// Sets preview window to winform panel hosted by xaml window
+			_deviceSource.PreviewWindow = new PreviewWindow(new HandleRef(panel1, panel1.Handle));
+
+			// Make this source the active one
+			_job.ActivateSource(_deviceSource);
+		}
+	}
+}
